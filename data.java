41f66657bda6d93150069b2165157dd8929020ca
trian-del <110490125+trian-del@users.noreply.github.com>
2026-09-14 18:50:41 -0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,10 @@
+public class data{
+    public static void main(String[] args){
+        String day = "Monday";
+        String month = "September";
+        int date = 14;
+        int year = 2026;
+        System.out.println("American format: "+day+", "+month+" "+date+", "+year);
+        System.out.println("European format: "+day+" "+date+" "+month+" "+year);
+    }
+}
